fe454941a43f8c5c0a23b7e5e9b1eae70fd2ab75
GamerUnderDevelopment <gamerunderdevelopment@gmail.com>
2026-04-01 09:18:22 -0700

    TEST SKELETON

---
+++ src/app/__tests__/page.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import Page from '../page';
+
+describe('Landing Page: Character Builder', () => {
+  it.skip('render the main heading', () => {});
+  it.skip('render the character builder contrainer', () => {});
+  it.skip('render the initial form elements', () => {});
+  it.skip('matches the snapshot', () => {});
+});
