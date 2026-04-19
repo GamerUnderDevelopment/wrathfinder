c126670a390849115aefeac8325aeb565d910c9a
GUD <gamerunderdevelopment@gmail.com>
2026-04-19 00:27:40 -0700

    [EP-01-S01-T01] Skeleton: All tests for Vitest and React Testing Library setup

---
+++ src/test/setup.test.ts
@@ -0,0 +1,6 @@
+import { describe, it } from "vitest";
+
+describe("Vitest setup", () => {
+  it.skip("should run a test successfully", () => {});
+  it.skip("should have access to jest-dom matchers", () => {});
+});
