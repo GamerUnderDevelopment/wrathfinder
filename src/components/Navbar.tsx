7b5742844b074b44098bc0ba1a0429bea451671a
GUD <gamerunderdevelopment@gmail.com>
2026-04-26 21:01:27 -0700

    [EP-01-S01-T02] Implement: Render Wrathfinder logo image - GREEN

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 const Navbar = () => {
-  return <nav></nav>;
+  return (
+    <nav>
+      <Image
+        src="/images/logo-icon.png"
+        alt="Wrathfinder Logo"
+        width={40}
+        height={40}
+      />
+    </nav>
+  );
 };
 
 export default Navbar;
