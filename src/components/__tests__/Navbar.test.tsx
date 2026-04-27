9c006d9b8621c362b5a9a579fbea37d9f7a1ceb6
GUD <gamerunderdevelopment@gmail.com>
2026-04-26 18:14:35 -0700

    [EP-01-S01-T02] Skeleton: All tests for Navbar component

---
+++ src/components/__tests__/Navbar.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it } from "vitest";
+
+describe("Navbar", () => {
+  it.skip("should render the Wrathfinder logo", () => {});
+  it.skip("should render the Wrathfinder wordmark", () => {});
+  it.skip("should render the navigation bar", () => {});
+});
