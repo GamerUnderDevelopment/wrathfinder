0ccebdfdc6204fe00b1432a749acb965bc3e83f5
GUD <gamerunderdevelopment@gmail.com>
2026-04-26 22:15:48 -0700

    [EP-01-S01-T02] Test: should render the Wrathfinder wordmark - RED

--- src/components/__tests__/Navbar.test.tsx
+++ src/components/__tests__/Navbar.test.tsx
@@ -6,7 +6,12 @@ describe("Navbar", () => {
   it("should render the Wrathfinder logo", () => {
     render(<Navbar />);
     const logo = screen.getByRole("img", { name: /wrathfinder logo/i });
+    expect(logo).toBeInTheDocument();
   });
-  it.skip("should render the Wrathfinder wordmark", () => {});
+  it("should render the Wrathfinder wordmark", () => {
+    render(<Navbar />);
+    const wordmark = screen.getByRole("img", { name: /wrathfinder wordmark/i });
+    expect(wordmark).toBeInTheDocument();
+  });
   it.skip("should render the navigation bar", () => {});
 });
