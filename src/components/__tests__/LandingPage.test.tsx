47970f1364273eaf5f258e3645d241cc003dce61
GUD <gamerunderdevelopment@gmail.com>
2026-04-28 07:20:24 -0700

    [EP-01-S01-T03] Skeleton: All tests for landing page

---
+++ src/components/__tests__/LandingPage.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe("LandingPage", () => {
+  it.skip("should render the navigatrion bar", () => {});
+  it.skip("should render a call to action to create a character", () => {});
+  it.skip("should render a call to action to sign in", () => {});
+});
