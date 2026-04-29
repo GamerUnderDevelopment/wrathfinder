d34277096c82b5cc9c4b5de13474f81c41ca493e
GUD <gamerunderdevelopment@gmail.com>
2026-04-28 19:14:11 -0700

    [EP-01-S01-T03] Skeleton: Update landing page tests to scope page.tsx only

---
+++ src/app/__tests__/page.test.tsx
@@ -0,0 +1,6 @@
+import { describe, it } from "vitest";
+
+describe("Landing Page /", () => {
+  it.skip("should render a call to action to create a character", () => {});
+  it.skip("should render a call to action to sign in", () => {});
+});
