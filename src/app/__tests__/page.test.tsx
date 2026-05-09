f3e7292daf4367132337e3832dbc971d686a1fbf
GUD <gamerunderdevelopment@gmail.com>
2026-05-09 15:25:40 -0700

    [EP-01-S01-T06] Skeleton: Add navbar test to landing page test suite

--- src/app/__tests__/page.test.tsx
+++ src/app/__tests__/page.test.tsx
@@ -3,6 +3,9 @@ import { render, screen } from "@testing-library/react";
 import Home from "../page";
 
 describe("Landing Page /", () => {
+  it.skip("should render the navigation bar", () => {
+    //
+  });
   it("should render a call to action to create a character", () => {
     render(<Home />);
     const cta = screen.getByRole("link", { name: /create a character/i });
